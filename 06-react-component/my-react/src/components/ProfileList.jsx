74efd2a3593950ad4e062b2f884b3a3b910f676a
kwonboryong <kwonboryong@naver.com>
2024-07-27 20:02:25 +0900

    Fix: 피드백 반영에 따른 수정

--- 06-react-component/my-react/src/components/ProfileList.jsx
+++ 06-react-component/my-react/src/components/ProfileList.jsx
@@ -11,10 +11,10 @@ function ProfileList() {
         <Profile name="슬비쌤" img="profile-1.jpg" description="슬비쌤 프로필" />
       </li>
       <li>
-        <Profile name="야무" img="profile-2.jpg" description="범쌤 프로필" />
+        <Profile name="야무" img="profile-2.jpg" description="야무쌤 프로필" />
       </li>
       <li>
-        <Profile name="범쌤" img="profile-3.jpg" description="야무쌤 프로필" />
+        <Profile name="범쌤" img="profile-3.jpg" description="범쌤 프로필" />
       </li>
       <li>
         <Profile name="이듬" img="profile-4.jpg" description="이듬 프로필" />
