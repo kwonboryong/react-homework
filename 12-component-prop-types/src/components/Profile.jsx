6ebe44221da06ef055ba9f9a8c04bf7bd5a7f3bb
Bo_Ryong <152785122+kwonboryong@users.noreply.github.com>
2024-08-03 21:46:27 +0900

    update: 파일명 변경에 따른 ProfileType 경로 변경

--- 12-component-prop-types/src/components/Profile.jsx
+++ 12-component-prop-types/src/components/Profile.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './../main.css'
 import PropTypes from 'prop-types';
-import { ProfileType } from '@/@types/types';
+import { ProfileType } from '@/@types/types.d';
 
 // 속성 타입 검사
 Profile.propTypes = ProfileType;
